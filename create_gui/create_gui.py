03c0350b5bfa250100b388a83ad723c308ad5e65
jenisha987 <jensastha987@gmail.com>
2024-05-27 19:27:56 +0545

    created gui

--- create_gui/create_gui.py
+++ create_gui/create_gui.py
@@ -4,7 +4,9 @@ root = Tk()
 
 #Creating a Label Widget
 myLabel = Label(root, text="Hello World!")
+name = Label(root, text="John Dan")
 #Shoving it onto the screen
 myLabel.pack()
+name.pack()
 
 root.mainloop()
